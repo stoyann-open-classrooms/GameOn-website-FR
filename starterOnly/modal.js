70519774f9a6b1c0dcee68766d6338a04f990e2a
Stoyann <stoyann.openclassrooms@gmail.com>
2021-02-26 16:54:09 +1100

    #1: fermeture de la modale (Ajout de la fonctionalité au boutonX

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,34 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close =document.querySelector (".close");
+
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal
+
+close.addEventListener("click", closeModal);
+
+
+
+
+// Fonctions
+
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
-}
+};
+
+
+function closeModal() {
+  modalbg.style.display = "none";
+ 
+};
+
+
 
 
