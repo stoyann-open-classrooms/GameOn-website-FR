b25828101da78ff989c24d18077e15d9f3acccf8
Stoyann <stoyann.openclassrooms@gmail.com>
2021-03-15 17:49:11 +1100

    mise en place des fonctions de verifications

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -6,66 +6,33 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const validationOk = document.querySelector('.reservation-ok');
 const close = document.querySelector (".close");
 const closeBtn = document.querySelectorAll(".close, .btn-close");
-const submitBTN = document.querySelector(".btn-submit");
 const errorAlert = document.querySelectorAll(".message-alerte")
 const iconeBurger = document.querySelector(".icon");
+const anneeDeNaissance = document.querySelector("#birthdate")
 
 
-
-
-
-form.first.addEventListener("input", (e) => {
-
-  if(e.target.value.length < 2){
-    errorAlert[0].style.display = "block"
-  }else {
-    
-    errorAlert[0].style.display = "none"
-    return true
-    
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-})
+}
 
 
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
 
-form.last.addEventListener("input", (e) => {
-
-  if(e.target.value.length < 2){
-    errorAlert[1].style.display = "block"
-  }else {
-    
-    errorAlert[1].style.display = "none"
-    return true
-    
-  }
-})
-
-
-
-
-
-// ecouter les modifications de l'input mail
-
-form.email.addEventListener('change', function() {
-  validEmail(this);
-})
-
-
-// verification de l'email (renvoie true ou false)
-
-const validEmail = function(inputEmail){
-  let emailRegex= new RegExp (/^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i);
-
-  let testEmail = emailRegex.test(inputEmail.value);
-  if(testEmail == false) {
-    
-    errorAlert[2].style.display = "block"
-    
-  } else{
-    
-    errorAlert[2].style.display = "none"
-  };
 };
+
+
+function closeModal() {
+  modalbg.style.display = "none";
+
+};
+
 
 
 
@@ -81,100 +48,118 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 
-// Fonctions
 
 
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
+
+
+
+
+function verifFirst() {
+ 
+  if (form.first.value === "" || form.first.length < 2){
+  
+    errorAlert[0].style.display = "block";
+    
+    return false;
+  
+  } else { 
+    errorAlert[0].style.display = "none";
+    return  true
   }
+
+}
+
+
+
+function verifLast() {
+
+  if (form.last.value === "" || form.last.value < 2) {
+  
+    errorAlert[1].style.display = "inline";
+   
+    return false;
+  
+  } else {
+    errorAlert[1].style.display = "none";
+    return true
+
+  }
+}
+
+
+
+function verifMAil() {
+
+
+  // verification email
+  
+  let regexMail = /^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i;
+  
+  if(regexMail.exec(form.email.value) === null) {
+    
+    errorAlert[2].style.display = "block"; 
+    
+    return false;
+    
+  } else {
+    
+    errorAlert[2].style.display = "none";
+    return true
+    
+    
+  }
+  
+}
+
+
+// verification de la date
+
+ 
+// recuperation de la date du jour
+const todayDate = new Date().toISOString().split('T')[0];
+// recuperation de l'annee 
+const currentYear = new Date().getFullYear();
+// impossible de mettre une date dans le futur
+form.birthdate.max = todayDate;
+
+
+
+
+function verifTournament(alert) {
+
+
+  let inputTournament = document.querySelector("#quantity");
+  
+  if( inputTournament.value == "") {
+    
+    errorAlert[4].style.display = "block"; 
+    return false;
+    
+  } else {
+    
+    errorAlert[4].style.display = "none";
+    return true
+  }
+  
 }
 
 
 
 
 
+ let tournamentBool = verifTournament();
+ let firstbool= verifFirst();
+let lastBool = verifLast();
+ let mailBool = verifMAil();
 
 
+formulaire.addEventListener('submit' , (e) => {
 
+  e.preventDefault();
 
 
+validationOk.style.display="flex";
 
 
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-
-};
-
-
-function closeModal() {
-  modalbg.style.display = "none";
-
-};
-
-// comportement par defaut du submit none
- 
-submitBTN.addEventListener("submit",  (e) => {
-      
-  
- 
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ });
 
